bd8a63bb817ee236983cc21be6a2105720d30e7b
Shawn Clary <sclary@icloud.com>
2021-10-10 07:19:16 -0400

    finished implimenting PW reset emailer

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 require('dotenv').config()
 const mongoose = require('mongoose')
 const cors = require('cors')
-
 const app = express()
 const port = process.env.PORT || 3030;
 
@@ -33,6 +32,7 @@ mongoose.connection.once('connected', () => console.log('Connected to Mongo, lif
 app.use('/', require('./controllers/rootController'))
 app.use('/user', require('./controllers/userController'))
 app.use('/admin', require('./controllers/adminController'))
+app.use('/pwr', require('./controllers/pwrController'))
 
 //////////////
 // LISTENER //
